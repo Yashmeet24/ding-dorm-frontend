3a0f90446879d3ccfb1b4488bb8bf26740720551
Yashmeet Kaur <yashmeetkaur777@gmail.com>
2023-08-07 11:55:25 +0530

    added sidebar css

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,21 +3,40 @@ import { Link } from 'react-router-dom';
 const Sidebar = () => {
     return ( 
         <div className="sidebar">
-            <div className="Dashboard">
-                <img className="Dashboard-icon" src={require('./images/dashboard-solid-24.png')} alt="Dashboard icon" />
-                <h4><Link to="/Dashboard" className="Dashboard-title">Dashboard</Link></h4>
+            <input type="checkbox"/>
+            <div className="toggle">
+                <span className='top_line common'></span>
+                <span className='middle_line common'></span>
+                <span className='bottom_line common'></span>
             </div>
-            <div className="CleaningRequest">
-                <img className="CleaningRequest-icon" src={require('./images/cleaningreq.png')} alt="CleaningRequest icon" />
-                <h4><Link to="/CleaningRequestPage" className="CleaningRequest-title">Request for Cleaning</Link></h4>
-            </div>
-            <div className="RoomStatistics">
-                <img className="RoomStatistics-icon" src={require('./images/roomstatus.png')} alt="RoomStatistics icon" />
-                <h4><Link to="/RoomStatisticsPage" className="RoomStatistics-title">Room Statistics</Link></h4>
-            </div>
-            <div className="Feedback">
-                <img className="Feedback-icon" src={require('./images/feedback.png')} alt="Feedback icon" />
-                <h4><Link to="/FeedbackPage" className="Feedback-title">Feedback</Link></h4>
+            <div className="slide">
+                <div className="logo d-flex m-4">
+                    <img className="logoimage rounded-4" src={require('./images/dingdormlogo-blue5.png')} alt="logo" />
+                </div>
+                <div className="Dashboard">
+                    <img className="sidebaricons Dashboard-icon" src={require('./images/dashboard-solid-24.png')} alt="Dashboard icon" />
+                    <Link className="sidebarTitles Dashboard-title" to="/Dashboard" >Dashboard</Link>
+                </div>
+                <div className="CleaningRequest">
+                    <img className="sidebaricons CleaningRequest-icon" src={require('./images/cleaningreq.png')} alt="CleaningRequest icon" />
+                    <Link className="sidebarTitles CleaningRequest-title" to="/CleaningRequestPage" >Request for Cleaning</Link>
+                </div>
+                <div className="RoomStatistics">
+                    <img className="sidebaricons RoomStatistics-icon" src={require('./images/roomstatus.png')} alt="RoomStatistics icon" />
+                    <Link className="sidebarTitles RoomStatistics-title" to="/RoomStatisticsPage" >Room Statistics</Link>
+                </div>
+                <div className="Feedback">
+                    <img className="sidebaricons Feedback-icon" src={require('./images/feedback.png')} alt="Feedback icon" />
+                    <Link className="sidebarTitles Feedback-title" to="/FeedbackPage" >Feedback</Link>
+                </div>
+                <div className="settings">
+                    <img className="sidebaricons settings-icon" src={require('./images/setting.png')} alt="settings icon" />
+                    <Link className="sidebarTitles settings-title" to="/settingsPage" >Settings</Link>
+                </div>
+                <div className="Logout">
+                    <img className="sidebaricons logout-icon" src={require('./images/exit.png')} alt="logout icon" />
+                    <Link className="sidebarTitles logout-title" to="/logoutPage" >Log out</Link>
+                </div>
             </div>
         </div>
      );
